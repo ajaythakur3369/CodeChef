cb3ca9877537c7d5537cdd63fe70efb8b84bbad1
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-30 10:48:19 +0530

    Add files via upload

---
+++ 30_January_2024.py
@@ -0,0 +1,18 @@
+'''
+Task:- 
+Rearrange the code so that the code prints the numbers from 1 to 10.
+'''
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+'''
+Answer:- 
+i = 1
+while i <= 10:
+    print(i, end=", ")
+    i = i + 1
+'''
+
