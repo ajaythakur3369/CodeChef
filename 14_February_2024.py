8e02899306cabe9fe58511876b2151683283413e
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-14 16:47:11 +0530

    Add files via upload

---
+++ 14_February_2024.py
@@ -0,0 +1,23 @@
+'''
+Task:- 
+Printing a number
+Let us output a number in Python.
+
+Some code has been populated in the IDE on the right.
+This code just prints 12 on the screen when you run it.
+
+Click on Submit below the IDE to know the result. Then click on next to continue.
+'''
+
+print(12)
+
+'''
+Task:- 
+Replace the _ (underscores) in the editor with the text I love Python and Submit.
+'''
+
+'''
+Replace print(___) with print("I love Python")
+'''
+
+print("I love Python")
