91364cb9d24d2ff96c902ad6898ed410f168b593
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-16 10:54:47 +0530

    Add files via upload

---
+++ 15_February_2024.py
@@ -0,0 +1,13 @@
+'''
+Task:- 
+In the editor we have written half code for you. Some portion of the code contains __ (underscores). Remove those underscore and write 21 in place of first underscore and 40 in place of the next underscore.
+To pass this exercise, your code has to print the sum of 21 and 40.
+--
+You may have noticed the 1st line in the editor having instructions for you. These lines are not code, they are called comments. Comments in Python start with a #. You can write anything inside a comment.
+'''
+
+'''
+Replace the code print(__ + __) with print(21 + 40)
+'''
+
+print(21 + 40)
---
+++ 16_February_2024.py
@@ -0,0 +1,20 @@
+'''
+Task:- 
+Which line of code will output the text Hi?
+'''
+
+'''
+Which line of code will output the string "Hi"?
+'''
+
+(A) print Hi
+
+(B) print(Hi);
+
+(C) print "Hi"
+
+(D) print("Hi")
+
+'''
+Output:- (D) print("Hi")
+'''
