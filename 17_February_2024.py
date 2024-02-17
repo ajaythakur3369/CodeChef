13039f8d310444c411462e36f5f6bb616891b94f
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-17 11:28:14 +0530

    Add files via upload

---
+++ 17_February_2024.py
@@ -0,0 +1,21 @@
+'''
+Task:- 
+Which lines of code will output the sum of 7 and 19?
+'''
+
+'''
+Which line of code will output the sum of 7 and 19?
+Hint: There are two correct answers.
+'''
+
+(A) print = 7 + 19
+
+(B) print(7 + 19)
+
+(C) print("7 + 19")
+
+(D) print (7 + 19)
+
+'''
+Output:- (B) and (D) are correct.
+'''
