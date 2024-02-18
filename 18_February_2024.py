e58db781c5a20551652907aea59bd396c2ce48a7
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-18 17:56:03 +0530

    Add files via upload

---
+++ 18_February_2024.py
@@ -0,0 +1,19 @@
+'''
+Task:- 
+We can add as many print statements as we want.
+Write a program which does the following
+ Add a print statement and output the sum of 3 + 4
+ Add another print statement and output the sum of 2 + 1.
+Note - Notice that the output is printed on separate lines
+'''
+
+'''
+Update the '_' in the code below to output the following 
+7
+3
+'''
+
+print(3 + 4)
+
+print(2 + 1)
+
