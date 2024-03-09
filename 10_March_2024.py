7005d33660cb991d8912a4d1c0369e493b387b53
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-03-10 00:26:52 +0530

    Add files via upload

---
+++ 10_March_2024.py
@@ -0,0 +1,26 @@
+'''
+Task:- 
+Write a program which does the following:
+ Initialize a string variable word and assign the value Ocygen to it.
+ You now want to fix the typo in the given string.
+ Use the syntax explained above to replace 'c' with 'x' in a new variable word_new
+ Output the updated word_new to console
+'''
+
+'''
+Update the '_' in the code below to solve the problem
+'''
+
+word = "Ocygen"
+
+'''
+Fix the typo by replacing 'c' with 'x'
+'''
+
+word_new = word.replace('c', 'x')
+
+'''
+Output the updated word_new to console
+'''
+
+print(word_new)
